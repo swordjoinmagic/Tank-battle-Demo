836a85054664730956b550f88f51a6ccb66b2143
swordjoinmagic <2483787753@qq.com>
2018-09-16 10:45:03 +0800

    完成坦克控制单元

--- Scripts/TankControl/Axleinfo.cs
+++ Scripts/TankControl/Axleinfo.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 用来表示挂在一个轴上的左右车轮
+/// </summary>
 [System.Serializable]
 public class Axleinfo {
+
+    // 左轮
     public WheelCollider leftWheel;
+
+    // 右轮
     public WheelCollider rightWheel;
+
+    // 马力
     public bool motor;
+
+    // 用于指明轮子是否转向
     public bool steering;
 }
--- Scripts/TankControl/TankMoveControl.cs
+++ Scripts/TankControl/TankMoveControl.cs
@@ -19,13 +19,96 @@ public class TankMoveControl : MonoBehaviour {
     private float steering = 0;
     public float maxSteeringAngle;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+    private Rigidbody rigid;
+    public Transform centerRigidbody;
+
+    // 轮子
+    private Transform wheels;
+
+    // 履带
+    private Transform tracks;
+
+    /// <summary>
+    /// 用于控制玩家移动
+    /// </summary>
+    public void PlayerCtrul() {
+        // 获得马力和转向角
+        motor = maxMotorTorque * Input.GetAxis("Vertical");
+        steering = -maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        // 制动
+        breakTorque = 0;
+        foreach (var axlenInfo in axleinfos) {
+            if (axlenInfo.leftWheel.rpm > 5 && motor < 0) {
+                breakTorque = maxMotorTorque;
+            } else if (axlenInfo.leftWheel.rpm < -5 && motor > 0) {
+                breakTorque = maxMotorTorque;
+            }
+            
+        }
+    }
+
+    /// <summary>
+    /// 轮子旋转
+    /// </summary>
+    /// <param name="collider"></param>
+    public void WheelsRotation(WheelCollider collider) {
+        Vector3 posiiton;
+        Quaternion rotation;
+        collider.GetWorldPose(out posiiton,out rotation);
+        foreach (Transform wheel in wheels) {
+            wheel.rotation = rotation;
+        }
+    }
+
+    public void TrackMove() {
+        float offset = 0;
+        offset = wheels.GetChild(0).localEulerAngles.x / 90f;
+        foreach (Transform track in tracks) {
+
+            // 获取材质
+            MeshRenderer mr = track.gameObject.GetComponent<MeshRenderer>();
+
+            if (mr == null) continue;
+
+            Material material = mr.material;
+            material.mainTextureOffset = new Vector2(0,offset);
+        }
+    }
+
+    private void Start() {
+        rigid = GetComponent<Rigidbody>();
+        //rigid.centerOfMass = centerRigidbody.position;
+
+        wheels = transform.Find("wheels");
+        tracks = transform.Find("tracks");
+    }
+
+    // Update is called once per frame
+    void Update () {
+        PlayerCtrul();
+
+        // 遍历车轴
+        foreach (var axleInfo in axleinfos) {
+
+            // 设置转向
+            if (axleInfo.steering) {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+
+            // 设置马力
+            if (axleInfo.motor) {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+
+            axleInfo.leftWheel.brakeTorque = breakTorque;
+            axleInfo.rightWheel.brakeTorque = breakTorque;
+
+            // 转动轮子
+            WheelsRotation(axleInfo.leftWheel);
+            TrackMove();
+        }
 	}
 }
